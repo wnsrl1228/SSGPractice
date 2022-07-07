a36d829b35f5e790975419aedef9e4d8438c7719
wnsrl1228 <wnsrl981228@gmail.com>
2022-07-07 15:12:43 +0900

    ëë등록추가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -7,12 +7,19 @@ public class App {
         System.out.println("== 명언 SSG ==");
 
         Scanner sc = new Scanner(System.in);
-
+        int i = 1;
         outer:while ( true ){
             System.out.printf("명령) ");
             String cmd = sc.nextLine().trim();
 
             switch (cmd) {
+                case "등록":
+                    System.out.printf("명언 : ");
+                    String say = sc.nextLine();
+                    System.out.printf("작가 : ");
+                    String author = sc.nextLine();
+                    System.out.println(i +"번 명언이 등록되었습니다.");
+                    break;
                 case "종료":
                     break outer;
             }
