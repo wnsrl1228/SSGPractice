d4bcead38b04324129e725afe2e51f1cd657fed9
wnsrl1228 <wnsrl981228@gmail.com>
2022-07-08 14:44:43 +0900

    입력받은 목록을 리스트에 추가, 목록기능 추가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,5 +1,7 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 //# 요구사항
 //          - 영속성
@@ -38,6 +40,8 @@ public class App {
         System.out.println("== 명언 SSG ==");
 
         Scanner sc = new Scanner(System.in);
+
+        List<WiseSaying> wiseSayings = new ArrayList<>();
         int wiseSayinglastId = 0;
         outer:while ( true ){
             System.out.printf("명령) ");
@@ -51,12 +55,20 @@ public class App {
                     String author = sc.nextLine();
                     int id = ++wiseSayinglastId;
                     WiseSaying wiseSaying = new WiseSaying(id, content, author);
-                    System.out.println(wiseSaying);
+                    wiseSayings.add(wiseSaying);
                     System.out.println(id +"번 명언이 등록되었습니다.");
                     break;
                 case "삭제":
                 case "수정":
                 case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("-------------------");
+                    for (int i = wiseSayings.size()-1; i >=0 ; i--) {
+                        WiseSaying wiseSaying_ = wiseSayings.get(i);
+                        System.out.printf("%d / %s / %s\n", wiseSaying_.id,wiseSaying_.content,wiseSaying_.author);
+
+                    }
+                    break;
                 case "종료":
                     break outer;
             }
