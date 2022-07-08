453302b3f6180f62b5da2d6029e272423bd61c7b
wnsrl1228 <wnsrl981228@gmail.com>
2022-07-08 14:36:49 +0900

    표준출력을_리다이렉션하여_결과를_문자열로_받기

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -2,8 +2,7 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,7 +14,7 @@ public class AppTest {
         assertEquals(30, rs);
     }
     @Test
-    public void 테스트_스캐너(){
+    public void 문자열을_스캐너의_입력으로_설정(){
         String input = """
                 등록
                 명언1
@@ -27,8 +26,26 @@ public class AppTest {
         String cmd = sc.nextLine().trim();
         String content = sc.nextLine().trim();
         String author = sc.nextLine().trim();
+
         assertEquals("등록",cmd);
         assertEquals("명언1",content);
         assertEquals("작가1",author);
+    }
+
+    @Test
+    public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
+        // 표준출력 리다이렉션
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕");
+
+        String rs = output.toString().trim();
+
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        output.close();
+
+        assertEquals("안녕", rs);
+
     }
 }
