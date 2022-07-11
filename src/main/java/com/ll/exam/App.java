906683db06730e76bae95b31f3acac1ad31bfe02
wnsrl1228 <wnsrl981228@gmail.com>
2022-07-11 15:25:51 +0900

    findById 메서드 도입

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -3,42 +3,11 @@ package com.ll.exam;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-//# 요구사항
-//          - 영속성
-//        - DB 사용금지
-//        - 파일사용가능
-//        - JSON 형식 추천
-//        - 실행예시
-//        명령) 등록
-//        명언 : 현재를 사랑하라.
-//        작가 : 작자미상
-//        1번 명언이 등록되었습니다.
-//        명령) 등록
-//        명언 : 과거에 집착하지 마라.
-//        작가 : 작자미상
-//        2번 명언이 등록되었습니다.
-//        명령) 목록
-//        번호 / 작가 / 명언
-//        ----------------------
-//        2 / 작자미상 / 과거에 집착하지 마라.
-//        1 / 작자미상 / 현재를 사랑하라.
-//        명령) 삭제?id=1
-//        1번 명언이 삭제되었습니다.
-//        명령) 삭제?id=1
-//        1번 명언은 존재하지 않습니다.
-//        명령) 수정?id=2
-//        2번 명언을 수정합니다.
-//        기존 명언 : 과거에 집착하지 마라.
-//        새 명언 : 미래와 과거에 집착하지 마라.
-//        2번 명언이 수정되었습니다.
-//        명령) 목록
-//        번호 / 작가 / 명언
-//        ----------------------
-//        2 / 작자미상 / 미래와 과거에 집착하지 마라.
+
 public class App {
-    Scanner sc;
-    List<WiseSaying> wiseSayings;
-    int wiseSayinglastId;
+    private Scanner sc;
+    private List<WiseSaying> wiseSayings;
+    private int wiseSayinglastId;
     public App() {
         sc = new Scanner(System.in);
         wiseSayings = new ArrayList<>();
@@ -47,9 +16,6 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 SSG ==");
-
-
-
 
         outer:while ( true ){
             System.out.printf("명령) ");
@@ -94,14 +60,8 @@ public class App {
             return;
         }
         // URL에 입력된 id에 해당하는 명언객체 찾기
-        WiseSaying foundWiseSaying = null;
+        WiseSaying foundWiseSaying = findById(paramId);
 
-        for (WiseSaying wiseSaying : wiseSayings) {
-            if (wiseSaying.id == paramId){
-                foundWiseSaying = wiseSaying;
-                break;
-            }
-        }
         // 존재하는 id값이 없을 경우 중지
         if (foundWiseSaying == null) {
             System.out.printf("%d번 명언은 존재하지 않습니다..\n", paramId);
@@ -112,6 +72,16 @@ public class App {
 
         System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
     }
+
+    private WiseSaying findById(int paramId) {
+        for (WiseSaying wiseSaying : wiseSayings) {
+            if (wiseSaying.id == paramId){
+                return wiseSaying;
+            }
+        }
+        return null;
+    }
+
     private void write() {
         System.out.printf("명언 : ");
         String content = sc.nextLine();
