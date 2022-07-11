713822d13b96becfc70ec25b315ea9411d3ac59f
wnsrl1228 <wnsrl981228@gmail.com>
2022-07-11 14:26:07 +0900

    url 처리기 Rq 클래스 구현

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -8,6 +8,20 @@ import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
+    @Test void Rq__getIntParam(){
+        Rq rq = new Rq("삭제?id=1");
+        int id = rq.getIntParam("id", 0);
+
+        assertEquals(1, id);
+    }
+    @Test void Rq__getIntParam2(){
+        Rq rq = new Rq("삭제?id=10&no=1");
+        int id = rq.getIntParam("id", 0);
+        int no = rq.getIntParam("no", 0);
+
+        assertEquals(10, id);
+        assertEquals(1, no);
+    }
     @Test
     public void 테스트_실험(){
         int rs = 10+20;
