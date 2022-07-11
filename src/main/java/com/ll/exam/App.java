fb5bb75aaa210f9ea37690c3025a191249907936
wnsrl1228 <wnsrl981228@gmail.com>
2022-07-11 14:50:03 +0900

    삭제 기능 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -46,8 +46,8 @@ public class App {
         outer:while ( true ){
             System.out.printf("명령) ");
             String cmd = sc.nextLine().trim();
-
-            switch (cmd) {
+            Rq rq = new Rq(cmd);
+            switch (rq.getPath()) {
                 case "등록":
                     System.out.printf("명언 : ");
                     String content = sc.nextLine();
@@ -59,6 +59,34 @@ public class App {
                     System.out.println(id +"번 명언이 등록되었습니다.");
                     break;
                 case "삭제":
+                    //url에서 입력된 id값 얻기
+                    int paramId = rq.getIntParam("id",0);
+
+                    // url에 입력된 id가 없는 경우 중지
+                    if(paramId == 0){
+                        System.out.println("id를 입력해주세요");
+                        continue;
+                    }
+                    // URL에 입력된 id에 해당하는 명언객체 찾기
+                    WiseSaying wiseSaying__ = null;
+
+                    for (WiseSaying wiseSaying___ : wiseSayings) {
+                        if (wiseSaying___.id == paramId){
+                            wiseSaying__ = wiseSaying___;
+                            break;
+                        }
+                    }
+                    // 존재하는 id값이 없을 경우 중지
+                    if (wiseSaying__ == null) {
+                        System.out.printf("%d번 명언은 존재하지 않습니다..\n", paramId);
+                        continue;
+                    }
+                    // 입력된 id에 해당하는 명언객체를 리스트에서 삭제
+                    wiseSayings.remove(wiseSaying__);
+
+                    System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
+
+                    break;
                 case "수정":
                 case "목록":
                     System.out.println("번호 / 작가 / 명언");
--- src/main/java/com/ll/exam/Rq.java
+++ src/main/java/com/ll/exam/Rq.java
@@ -7,6 +7,9 @@ public class Rq {
     }
     public int getIntParam(String paramName, int defaultValue){
         String[] urlBits = url.split("\\?",2);
+        if (urlBits.length == 1) {
+            return defaultValue;
+        }
         urlBits = urlBits[1].split("&");
 
         for (String urlBit : urlBits) {
