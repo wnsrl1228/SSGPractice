91386b68eda4104d474113a870b53e1b5040abbb
wnsrl1228 <wnsrl981228@gmail.com>
2022-07-11 15:20:40 +0900

    App리팩토링

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -36,66 +36,37 @@ import java.util.Scanner;
 //        ----------------------
 //        2 / 작자미상 / 미래와 과거에 집착하지 마라.
 public class App {
+    Scanner sc;
+    List<WiseSaying> wiseSayings;
+    int wiseSayinglastId;
+    public App() {
+        sc = new Scanner(System.in);
+        wiseSayings = new ArrayList<>();
+        wiseSayinglastId = 0;
+    }
+
     public void run() {
         System.out.println("== 명언 SSG ==");
 
-        Scanner sc = new Scanner(System.in);
 
-        List<WiseSaying> wiseSayings = new ArrayList<>();
-        int wiseSayinglastId = 0;
+
+
         outer:while ( true ){
             System.out.printf("명령) ");
             String cmd = sc.nextLine().trim();
             Rq rq = new Rq(cmd);
             switch (rq.getPath()) {
                 case "등록":
-                    System.out.printf("명언 : ");
-                    String content = sc.nextLine();
-                    System.out.printf("작가 : ");
-                    String author = sc.nextLine();
-                    int id = ++wiseSayinglastId;
-                    WiseSaying wiseSaying = new WiseSaying(id, content, author);
-                    wiseSayings.add(wiseSaying);
-                    System.out.println(id +"번 명언이 등록되었습니다.");
+                    write();
                     break;
                 case "삭제":
-                    //url에서 입력된 id값 얻기
-                    int paramId = rq.getIntParam("id",0);
-
-                    // url에 입력된 id가 없는 경우 중지
-                    if(paramId == 0){
-                        System.out.println("id를 입력해주세요");
-                        continue;
-                    }
-                    // URL에 입력된 id에 해당하는 명언객체 찾기
-                    WiseSaying wiseSaying__ = null;
-
-                    for (WiseSaying wiseSaying___ : wiseSayings) {
-                        if (wiseSaying___.id == paramId){
-                            wiseSaying__ = wiseSaying___;
-                            break;
-                        }
-                    }
-                    // 존재하는 id값이 없을 경우 중지
-                    if (wiseSaying__ == null) {
-                        System.out.printf("%d번 명언은 존재하지 않습니다..\n", paramId);
-                        continue;
-                    }
-                    // 입력된 id에 해당하는 명언객체를 리스트에서 삭제
-                    wiseSayings.remove(wiseSaying__);
-
-                    System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
-
+                    remove(rq);
                     break;
                 case "수정":
-                case "목록":
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("-------------------");
-                    for (int i = wiseSayings.size()-1; i >=0 ; i--) {
-                        WiseSaying wiseSaying_ = wiseSayings.get(i);
-                        System.out.printf("%d / %s / %s\n", wiseSaying_.id,wiseSaying_.content,wiseSaying_.author);
+                    break;
 
-                    }
+                case "목록":
+                    list();
                     break;
                 case "종료":
                     break outer;
@@ -103,5 +74,52 @@ public class App {
         }
 
         sc.close();
+    }
+
+    private void list() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("-------------------");
+        for (int i = wiseSayings.size()-1; i >=0 ; i--) {
+            WiseSaying wiseSaying_ = wiseSayings.get(i);
+            System.out.printf("%d / %s / %s\n", wiseSaying_.id,wiseSaying_.content,wiseSaying_.author);
+        }
+    }
+    private void remove(Rq rq) {
+        //url에서 입력된 id값 얻기
+        int paramId = rq.getIntParam("id",0);
+
+        // url에 입력된 id가 없는 경우 중지
+        if(paramId == 0){
+            System.out.println("id를 입력해주세요");
+            return;
+        }
+        // URL에 입력된 id에 해당하는 명언객체 찾기
+        WiseSaying foundWiseSaying = null;
+
+        for (WiseSaying wiseSaying : wiseSayings) {
+            if (wiseSaying.id == paramId){
+                foundWiseSaying = wiseSaying;
+                break;
+            }
+        }
+        // 존재하는 id값이 없을 경우 중지
+        if (foundWiseSaying == null) {
+            System.out.printf("%d번 명언은 존재하지 않습니다..\n", paramId);
+            return;
+        }
+        // 입력된 id에 해당하는 명언객체를 리스트에서 삭제
+        wiseSayings.remove(foundWiseSaying);
+
+        System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
+    }
+    private void write() {
+        System.out.printf("명언 : ");
+        String content = sc.nextLine();
+        System.out.printf("작가 : ");
+        String author = sc.nextLine();
+        int id = ++wiseSayinglastId;
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings.add(wiseSaying);
+        System.out.println(id +"번 명언이 등록되었습니다.");
     }
 }
--- src/main/java/com/ll/exam/Rq.java
+++ src/main/java/com/ll/exam/Rq.java
@@ -2,17 +2,24 @@ package com.ll.exam;
 
 public class Rq {
     String url;
+    String path;
+    String queryStr;
     public Rq(String url){
         this.url = url;
+        String[] urlBits = url.split("\\?",2);
+        this.path = urlBits[0];
+
+        if(urlBits.length == 2) {
+            this.queryStr = urlBits[1];
+        }
     }
     public int getIntParam(String paramName, int defaultValue){
-        String[] urlBits = url.split("\\?",2);
-        if (urlBits.length == 1) {
+        if (queryStr == null) {
             return defaultValue;
         }
-        urlBits = urlBits[1].split("&");
+        String[] bits = queryStr.split("&");
 
-        for (String urlBit : urlBits) {
+        for (String urlBit : bits) {
             String[] paramNameAndValue = urlBit.split("=",2);
             String paramName_ = paramNameAndValue[0];
             String paramValue = paramNameAndValue[1];
@@ -26,7 +33,6 @@ public class Rq {
     }
 
     public String getPath() {
-        String[] urlBits = url.split("\\?",2);
-        return urlBits[0];
+        return path;
     }
 }
